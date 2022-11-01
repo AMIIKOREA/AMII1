024d8e367f4540091a1e9f66c78211e7ae72830f
AMII <yjkim@ibsimplant.com>
2022-11-01 15:08:35 +0900

    Initial commit

---
+++ app/src/main/java/world/amiikorea/signup_amii/MainActivity.kt
@@ -0,0 +1,11 @@
+package world.amiikorea.signup_amii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
